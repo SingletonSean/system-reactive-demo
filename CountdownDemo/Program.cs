a211f22d5fb14fca359c6d366e3ef257fa660c25
SingletonSean <singletonsean4@gmail.com>
2022-03-29 20:10:10 -0400

    Add basic countdown demo

---
+++ CountdownDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reactive.Linq;
+
+void Countdown(int seconds)
+{
+    Observable
+        .Timer(DateTimeOffset.UtcNow, TimeSpan.FromSeconds(1))
+        .Select(currentSeconds => seconds - currentSeconds)
+        .TakeWhile(currentSeconds => currentSeconds > 0)
+        .Subscribe((currentSeconds) =>
+        {
+            Console.WriteLine(currentSeconds);
+        }, 
+        () =>
+        {
+            Console.WriteLine("Blast off!");
+        });
+}
+
+Countdown(5);
+
+Console.ReadLine();
