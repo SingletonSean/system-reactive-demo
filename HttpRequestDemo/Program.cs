974a1e2b1a119293eef0346d35e18556ac92dd2f
SingletonSean <singletonsean4@gmail.com>
2022-04-05 20:56:39 -0400

    Add observable HTTP request

--- HttpRequestDemo/Program.cs
+++ HttpRequestDemo/Program.cs
@@ -1,10 +1,35 @@
 ﻿using HttpRequestDemo.Models;
 using HttpRequestDemo.Queries;
+using System.Reactive.Linq;
 
 CatFactQuery catFactQuery = new CatFactQuery();
 
-CatFact catFact = await catFactQuery.Execute();
-
-Console.WriteLine(catFact.Content);
+Observable
+    .FromAsync(() => catFactQuery.Execute())
+    .ValidateCatFactLength(80)
+    .Retry(3)
+    .Catch(Observable.Return(new CatFact() { Content = "Cats are cool." }))
+    .Subscribe((catFact) =>
+    {
+        Console.WriteLine(catFact.Content);
+    });
 
 Console.ReadLine();
+
+static class CatFactObservableExtensions
+{
+    public static IObservable<CatFact> ValidateCatFactLength(this IObservable<CatFact> observable, int maxLength)
+    {
+        return observable
+            .Select(catFact =>
+            {
+                if (catFact.Content.Length > maxLength)
+                {
+                    return Observable.Throw<CatFact>(new Exception("Cat fact was too long."));
+                }
+
+                return Observable.Return(catFact);
+            })
+            .Switch();
+    }
+};
